5f4a601e1cd443a63d68d69df7f4b23e19e1b7d5
luongducthang <luongthang4760@gmail.com>
2024-08-20 13:11:42 +0700

    update-migration

---
+++ database/migrations/2024_08_20_060428_create_letters_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('letters', function (Blueprint $table) {
+            $table->id();  
+            $table->string('title');  
+            $table->text('content');  
+            $table->foreignId('receiver_id')->constrained('users')->onDelete('cascade'); 
+            $table->foreignId('sender_id')->constrained('users')->onDelete('cascade'); 
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('letters');
+    }
+};

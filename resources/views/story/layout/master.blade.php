edcd2812f9bdcdf19b1cbeb1f679cc1117d0577c
Dao Duc Phong <daoducphong2004@gmail.com>
2024-08-31 22:26:55 +0700

    giao diện chi tiết tuyện và đọc truyện

---
+++ resources/views/story/layout/master.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    @include('home.layout.home.head')
+<body>
+    @include('home.layout.home.header')
+
+    @yield('content')
+
+    @include('home.layout.home.footer')
+</body>
+</html>
---
+++ resources/views/story/layout/readingmaster.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+    @include('story.layout.reading.head')
+    @include('story.layout.reading.header')
+
+    @yield('content')
+
+    @include('story.layout.reading.footer')
+</html>

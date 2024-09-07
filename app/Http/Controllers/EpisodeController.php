440a238c62eda65621250dca80cf0a5af700795a
Dao Duc Phong <daoducphong2004@gmail.com>
2024-09-07 18:25:27 +0700

    thêm sửa xóa truyện, episode và chapter,

--- app/Http/Controllers/EpisodeController.php
+++ app/Http/Controllers/EpisodeController.php
@@ -32,34 +32,33 @@ class EpisodeController extends Controller
     public function store(StoreepisodeRequest $request)
     {   //chưa viết vadidate
         // try {
-            // Validate the incoming request data
-            $validatedData = $request->validate([
-                'book_id' => 'required|integer|exists:books,id',
-                'title' => 'required|string|max:255',
-                'description' => 'required|string',
-                'episode_path' => 'required|file|mimes:png,jpg,jpeg,gif|max:2048', // Accept only specific image types and max 2MB
-            ]);
-            // Handle file upload
-            if ($request->hasFile('episode_path')) {
-                $file = $request->file('episode_path');
-                $filename = time() . '_' . $file->getClientOriginalName();
-                $filePath = $file->storeAs('public/episodes', $filename);
+        // Validate the incoming request data
+        $validatedData = $request->validate([
+            'book_id' => 'required|integer|exists:books,id',
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'episode_path' => 'required|file|mimes:png,jpg,jpeg,gif|max:2048', // Accept only specific image types and max 2MB
+        ]);
+        // Handle file upload
+        if ($request->hasFile('episode_path')) {
+            $file = $request->file('episode_path');
+            $filename = time() . '_' . $file->getClientOriginalName();
+            $filePath = $file->storeAs('public/episodes', $filename);
 
-                // Store episode data in the database
-                $episode = new Episode();
-                $episode->book_id = $validatedData['book_id'];
-                $episode->title = $validatedData['title'];
-                $episode->slug = ''; // Store the generated slug
-                $episode->description = $validatedData['description'];
-                $episode->episode_path = $filePath;
-                $episode->save();
-                $slug = Str::slug('t'.$episode->id.'-'.$validatedData['title']);
-                $episode->slug = $slug;
-                $episode->save();
+            // Store episode data in the database
+            $episode = new Episode();
+            $episode->book_id = $validatedData['book_id'];
+            $episode->title = $validatedData['title'];
+            $episode->slug = ''; // Store the generated slug
+            $episode->description = $validatedData['description'];
+            $episode->episode_path = $filePath;
+            $episode->save();
+            $slug = Str::slug('t' . $episode->id . '-' . $validatedData['title']);
+            $episode->slug = $slug;
+            $episode->save();
+        }
 
-            }
-
-            return redirect()->back()->with('episode', $episode);
+        return redirect()->back()->with('episode', $episode);
         // } catch (\Exception $e) {
         //     return redirect()->back()->with('error', 'An error occurred while creating the episode: ' . $e->getMessage());
         // }
@@ -68,11 +67,12 @@ class EpisodeController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(episode $episode)
+    public function show(episode $episode) {}
+    public function showU(string $slug)
     {
-
+        $episode = episode::where('slug', $slug)->with('chapters')->findOrFail();
+        dd($episode);
     }
-
     /**
      * Show the form for editing the specified resource.
      */
@@ -108,5 +108,4 @@ class EpisodeController extends Controller
             return response()->json(['success' => false, 'message' => 'Có lỗi xảy ra khi xóa tập. Vui lòng thử lại.']);
         }
     }
-
 }

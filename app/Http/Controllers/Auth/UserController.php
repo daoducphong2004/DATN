850d94e4451369e3dd1fceb12e7b3d57e0e17dbe
hoalt <hoaban2k4@gmail.com>
2024-09-18 00:30:56 +0700

    hiển thị thông tin đăng nhập avatar người dùng đăng nhập

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -196,7 +196,7 @@ class UserController extends Controller
 
     public function OLNDaDang()
     {
-        return view('user.OLNDaDang');
+        return view('user.OLNDaDang'); 
     }
 
     public function OLNThamGia()
--- resources/views/home/layout/home/header.blade.php
+++ resources/views/home/layout/home/header.blade.php
@@ -46,55 +46,7 @@
                 <a href="/" class="navbar-logo" style="background-image: url('{{ asset('img/logo-9.png') }}')"
                     title="Trang chủ"></a>
             </div>
-            <div id="navbar-user">
-                <div class="nav-user_icon">
-                    <div class="nav-user_avatar">
-                        <img src="{{ asset('img/noava.png') }}" alt="Your avatar">
-                    </div>
-                    <div class="at-user_avatar"></div>
-                    <ul class="account-sidebar hidden-block unstyled none">
-                        <li>
-                            <a href="/thanh-vien/199850"><i class="fas fa-user"></i><span>Tài khoản</span></a>
-                        </li>
-                        <li>
-                            <a href="/lich-su-doc"><i class="fas fa-history"></i><span>Lịch sử</span></a>
-                        </li>
-                        <li>
-                            <a href="/bookmark"><i class="fas fa-bookmark"></i><span>Đánh dấu</span></a>
-                        </li>
-                        <li>
-                            <a href="/ke-sach"><i class="fas fa-heart"></i><span>Kệ sách</span></a>
-                        </li>
-                        <li>
-                            <a href="/tin-nhan"><i class="fas fa-envelope"></i><span>Tin nhắn</span>
-                                <div class="at-user_list"></div>
-                            </a>
-                        </li>
-                        <hr class="none block-l">
-                        <li>
-                            <div class="nightmode-toggle">
-                                <i class="fas fa-moon"></i><span>Nền tối</span>
-                                <div class="toggle-icon"><i class="fas fa-toggle-off"></i></div>
-                            </div>
-                        </li>
-                        <li>
-                            <a href="/action"><i class="fas fa-cog"></i><span>Hệ thống</span></a>
-                        </li>
-                        <li>
-                            <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                                          document.getElementById('logout-form').submit();"
-                                class="link-underline link-underline-opacity-0"><i
-                                    class="fas me-2 fa-sign-out-alt"></i><span>Thoát</span></a>
 
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </li>
-                    </ul>
-                    </li>
-                </div>
-            </div>
 
             @if (!Auth::check())
                 <div id="navbar-user" class="guest">
@@ -102,6 +54,59 @@
                 </div>
                 <div id="navbar-user" class="guest">
                     <a class="login-link" href="{{ route('register') }}">Đăng ký</a>
+                </div>
+            @else
+                <div id="navbar-user" class="guest">
+                    <div id="navbar-user">
+                        <div class="nav-user_icon">
+                            <div class="nav-user_avatar">
+                                <img src="{{ asset(Auth::user()->avatar_url ?? 'img/noava.png') }}" alt="Your avatar">
+                            </div>
+                            <div class="at-user_avatar"></div>
+                            <ul class="account-sidebar hidden-block unstyled none">
+                                <li>
+                                    <a href="/thanh-vien/199850"><i class="fas fa-user"></i><span>Tài khoản</span></a>
+                                </li>
+                                <li>
+                                    <a href="/lich-su-doc"><i class="fas fa-history"></i><span>Lịch sử</span></a>
+                                </li>
+                                <li>
+                                    <a href="/bookmark"><i class="fas fa-bookmark"></i><span>Đánh dấu</span></a>
+                                </li>
+                                <li>
+                                    <a href="/ke-sach"><i class="fas fa-heart"></i><span>Kệ sách</span></a>
+                                </li>
+                                <li>
+                                    <a href="/tin-nhan"><i class="fas fa-envelope"></i><span>Tin nhắn</span>
+                                        <div class="at-user_list"></div>
+                                    </a>
+                                </li>
+                                <hr class="none block-l">
+                                <li>
+                                    <div class="nightmode-toggle">
+                                        <i class="fas fa-moon"></i><span>Nền tối</span>
+                                        <div class="toggle-icon"><i class="fas fa-toggle-off"></i></div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a href="/action"><i class="fas fa-cog"></i><span>Hệ thống</span></a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                              document.getElementById('logout-form').submit();"
+                                        class="link-underline link-underline-opacity-0"><i
+                                            class="fas me-2 fa-sign-out-alt"></i><span>Thoát</span></a>
+
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </li>
+                            </ul>
+                            </li>
+                        </div>
+                    </div>
+                    <a class="login-link">{{ Auth::user()->username}}</a>
                 </div>
             @endif
 

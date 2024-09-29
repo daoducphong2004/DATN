ff14203a9571dfd7b4de0b5137eef180eb549901
Dao Duc Phong <daoducphong2004@gmail.com>
2024-09-29 15:06:04 +0700

    a

--- routes/web.php
+++ routes/web.php
@@ -125,12 +125,12 @@ Route::prefix('admin')->group(function () {
     Route::put('/groups/update/{id}', [GroupController::class, 'update'])->name('groups_update');
     Route::delete('/groups/delete/{id}', [GroupController::class, 'destroy'])->name('groups_delete');
 
-    Route::get('/user', [ControllersUserController::class, 'index'])->name('user_index');
-    Route::get('/user/create', [ControllersUserController::class, 'create'])->name('user_create');
-    Route::post('/user/store', [ControllersUserController::class, 'store'])->name('user_store');
-    Route::get('/user/edit/{id}', [ControllersUserController::class, 'edit'])->name('user_edit');
-    Route::put('/user/update/{id}', [ControllersUserController::class, 'update'])->name('user_update');
-    Route::delete('/user/delete/{id}', [ControllersUserController::class, 'destroy'])->name('user_delete');
+    Route::get('/user', [UserController::class,'index'])->name('user_index');
+    Route::get('/user/create', [UserController::class, 'create'])->name('user_create');
+    Route::post('/user/store', [UserController::class, 'store'])->name('user_store');
+    Route::get('/user/edit/{id}', [UserController::class, 'edit'])->name('user_edit');
+    Route::put('/user/update/{id}', [UserController::class, 'update'])->name('user_update');
+    Route::delete('/user/delete/{id}', [UserController::class, 'destroy'])->name('user_delete');
 
 
     Route::get('/genres', [GenreController::class, 'index'])->name('genres_index');

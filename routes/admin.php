12b06d03e1ee37898ff480fa9d442308bc2fcc3b
Dao Duc Phong <daoducphong2004@gmail.com>
2024-10-23 12:03:13 +0700

    "Added new features to admin panel: user purchase history management, including list and detail views, and updated navigation menu"

--- routes/admin.php
+++ routes/admin.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Admin\StoryController;
 use App\Http\Controllers\Admin\UserController as AdminUserController;
 use App\Http\Controllers\Admin\BookCommentController as AdminBookCommentController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\admin\PurchaseManageController;
 use App\Http\Controllers\Admin\UserGroupController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\BookController;
@@ -111,6 +112,9 @@ Route::prefix('admin')->group(function () {
 
 
         Route::resource('/banners', BannerController::class);
+
+        Route::get('/purchase_history',[PurchaseManageController::class,'index'])->name('ListPurchaseUser');
+        Route::get('/purchase_history/{user}', [PurchaseManageController::class, 'showUserPurchasedChapters'])->name('detailPurchaseUser');
 
         // end phong
 

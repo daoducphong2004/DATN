3463fe27aaa9fe476105744cdf55d3c5fc2e3380
Hoalt29 <Hoaban2k4@gmail.com>
2024-10-25 23:22:59 +0700

    thay doi modal likes rating

--- app/Models/Like_rating.php
+++ app/Models/Like_rating.php
@@ -11,6 +11,8 @@ class Like_rating extends Model
     use HasFactory;
     use SoftDeletes;
 
+    protected $table = 'likes_rating';
+
     protected $fillable = [
         'user_id',
         'rating_id',

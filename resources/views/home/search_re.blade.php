e97da1331e4796f530f20c4e9323507ca784fa08
WolkerLeader <maoubaki@gmail.com>
2024-10-27 13:41:57 +0700

    update search

--- resources/views/home/search_re.blade.php
+++ resources/views/home/search_re.blade.php
@@ -1,7 +1,22 @@
 @if($data_book->isEmpty())
-    <p>Không tìm thấy kết quả nào.</p>
+<p>Không tìm thấy kết quả nào.</p>
 @else
-    @foreach($data_book as $book)
-        <p>{{ $book->title }}</p>
-    @endforeach
+@foreach($data_book as $item)
+<div class="">
+    <div class="">
+        <a href="{{ route('truyen.truyen', $item->slug) }}" title="{{ $item->title }}">
+            <div class="">
+                <div class=""
+                    style="background-image: url('{{ !empty($item->book_path) ? asset(Storage::url($item->book_path)) : asset('img/noava.png') }}">
+                </div>
+            </div>
+        </a>
+        <div class="">
+            <div class="" title="{{ $item->title }}"><a
+                    href="{{ route('truyen.truyen', $item->slug) }}">{{ $item->title }}</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endforeach
 @endif

2d1331809ca3f6895380aa7cedd7a41be0763ede
WolkerLeader <maoubaki@gmail.com>
2024-11-01 22:32:40 +0700

    update forum

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,7 +14,6 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         DB::table('categories')->insert([
-            ['content' => 'Tất cả', 'color' => '', 'slug' => 'tat-ca'],
             ['content' => 'Thông báo', 'color' => '#eb1d57', 'slug' => 'thong-bao'],
             ['content' => 'Tin tức', 'color' => '#e01bb4', 'slug' => 'tin-tuc'],
             ['content' => 'Hỏi đáp', 'color' => '#252eef', 'slug' => 'hoi-dap'],

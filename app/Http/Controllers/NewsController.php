4335037bf467dbbc38e42cb6fe6b4223ef974ea4
Hoalt29 <Hoaban2k4@gmail.com>
2024-11-04 22:40:36 +0700

    them moi tin tuc

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -18,7 +18,22 @@ class NewsController extends Controller
         return view('admin.news.add');
     }
 
-    public function newsHandleAdd(){
+    public function newsHandleAdd(Request $request){
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+            'author' => 'nullable|string|max:100',
+            'slug' => 'nullable|string|unique:news,slug|max:255',
+        ]);
 
+        // Tạo mới bài viết
+        News::create([
+            'title' => $request->input('title'),
+            'slug' => $request->input('slug'),
+            'content' => $request->input('content'),
+            'author' => $request->input('author'),
+        ]);
+
+        return redirect()->route('admin.news.index')->with('success', 'Tin tức đã được thêm thành công.');
     }
 }
--- resources/views/admin/news/add.blade.php
+++ resources/views/admin/news/add.blade.php
@@ -21,8 +21,35 @@
         </a>
         <form action="" method="POST">
             @csrf
-            <div class="row">
+            <div class="row mt-3">
+                <div class="col-6">
+                    <div class="form-group">
+                        <label for="">Tiêu đề tin tức</label>
+                        <input type="text" class="form-control" placeholder="Tiêu đề tin tức...." name="title">
+                        @error('title')
+                            <p style="color: red">{{$message}}</p>
+                        @enderror
+                    </div>
+                </div>
 
+                <div class="col-6">
+                    <div class="form-group">
+                        <label for="">Slug tin tức</label>
+                        <input type="text" class="form-control" placeholder="Slug tin tức...." name="slug">
+                    </div>
+                </div>
+
+                <div class="col-12">
+                    <label for="">Nội dung</label>
+                    <textarea name="content" cols="30" rows="10" placeholder="Nội dung tin tức" class="form-control"></textarea>
+                </div>
+
+                <div class="col-6 mt-3">
+                    <label for="">Tác giả</label>
+                    <input type="text" class="form-control" placeholder="Tác giả...." name="author">
+                </div>
+
+                <button class="btn btn-success pt-5" type="submit">Thêm mới</button>
             </div>
         </form>
     </div>
@@ -30,7 +57,35 @@
 
 @push('scripts')
     <script>
+       document.addEventListener('DOMContentLoaded', function() {
+            const titleInput = document.querySelector('input[name="title"]');
+            const slugInput = document.querySelector('input[name="slug"]');
 
+            function removeDiacritics(str) {
+                return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/đ/g, 'd').replace(/Đ/g, 'D');
+            }
+
+            titleInput.addEventListener('input', function() {
+                let slug = removeDiacritics(titleInput.value)
+                    .toLowerCase()
+                    .trim()
+                    .replace(/á|à|ả|ạ|ã|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ/gi, 'a') // Loại bỏ ký tự đặc biệt nhưng giữ lại chữ cái và số
+                    .replace(/é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/gi, 'e')
+                    .replace(/ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ/gi, 'o')
+                    .replace(/ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự/gi, 'u')
+                    .replace(/ý|ỳ|ỷ|ỹ|ỵ/gi, 'y')
+                    .replace(/đ/gi, 'd')
+                    .replace(/i|í|ì|ỉ|ĩ|ị/gi, 'i')
+                    .replace(/\`|\~|\!|\@|\#|\||\$|\%|\^|\&|\*|\(|\)|\+|\=|\,|\.|\/|\?|\>|\<|\'|\"|\:|\;|_/gi, '')
+                    .replace(/ /gi, "-")
+                    .replace(/\-\-\-\-\-/gi, '-')
+                    .replace(/\-\-\-\-/gi, '-')
+                    .replace(/\-\-\-/gi, '-')
+                    .replace(/\-\-/gi, '-')
+
+                slugInput.value = slug;
+            });
+        });
     </script>
 @endpush
 @push('styles')

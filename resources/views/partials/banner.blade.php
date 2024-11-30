badf8c46ad74d340e078207fc91a0ffa0571f4c6
LinhLinh9393 <linhlinh210217@gmail.com>
2024-11-30 09:46:18 +0700

    fix banner

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,6 +1,6 @@
 <a href="">
     <div class="index-background d-none d-lg-block"
-        style="background-image: url('')}}')"></div>
+        style="background-image: url('{{ Storage::url(!empty($pc_banner->image_path) ? $pc_banner->image_path : '') }}')"></div>
     <div class="index-background d-lg-none"
         style="background-image: url('{{ Storage::url(!empty($mobile_banner->image_path) ? $mobile_banner->image_path : '') }}'); background-size: cover">
     </div>

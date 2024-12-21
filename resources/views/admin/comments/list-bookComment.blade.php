7be959a5ea604e15ee1b9fcee36dd974c087abde
datcv2003 <datcvph31745@fpt.edu.vn>
2024-12-21 10:56:07 +0700

    datcv2003

--- resources/views/admin/comments/list-bookComment.blade.php
+++ resources/views/admin/comments/list-bookComment.blade.php
@@ -28,7 +28,7 @@
                         <thead>
                             <tr>
                                 <th>ID</th>
-                                <th>Chapter</th>
+                                <th>Truyện</th>
                                 <th>User</th>
                                 <th>Content</th>
                                 <th>Actions</th>
